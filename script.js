35e46d222c2a2ef58dd60c51a41cdb3e299d5a51
MoReza <mz.abdollahi223@gmail.com>
2021-03-07 22:32:02 +0330

    Bugs fixed

--- script.js
+++ script.js
@@ -3,38 +3,43 @@ const deleteBtn = document.querySelectorAll('a')
 const button = document.querySelector('#add-btn')
 const task = document.querySelector('#txt-task')
 const list = document.querySelector('#list')
+const err = document.querySelector('#err')
 
 button.addEventListener('click', e => {
     e.preventDefault()
+    if (task.value) {
+        const div = document.createElement('div')
+        div.className = 'list-item'
 
-    const div = document.createElement('div')
-    div.className = 'list-item'
+        const p = document.createElement('p')
+        p.innerText = task.value
 
-    const p = document.createElement('p')
-    p.innerText = task.value
+        const a = document.createElement('a')
+        const i = document.createElement('i')
 
-    const a = document.createElement('a')
-    const i = document.createElement('i')
+        i.classList.add('fas')
+        i.classList.add('fa-trash')
 
-    i.classList.add('fas')
-    i.classList.add('fa-trash')
+        a.appendChild(i)
 
-    a.appendChild(i)
+        a.className = 'delete-item'
+        a.setAttribute('href', '#')
 
-    a.className = 'delete-item'
-    a.setAttribute('href', '#')
+        a.addEventListener('click', e => {
+            e.preventDefault()
+            a.parentNode.remove()
+        })
 
-    a.addEventListener('click', e => {
-        e.preventDefault()
-        a.parentNode.remove()
-    })
+        div.appendChild(p)
+        div.appendChild(a)
 
-    div.appendChild(p)
-    div.appendChild(a)
+        list.appendChild(div)
 
-    list.appendChild(div)
-
-    task.value = null
+        task.value = null
+        err.innerText = null
+    } else {
+        err.innerText = "Please Enter your Task"
+    }
 })
 
 deleteBtn.forEach(btn => {
